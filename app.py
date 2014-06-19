a219fb2c740341dfbffcb89dfd7ed934ef8980eb
wangkechun <978750903@qq.com>
2014-06-19 19:07:03 +0800

    fix

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ logger = log.getloger()
 
 @app.route('/api/score/<int:id>')
 def getScore(id):
-    return route.getScore(id)
     try:
-        pass
+        return route.getScore(id)
     except Exception,e:
         logger.error('%s %s'%(id,e) )
         raise e
@@ -33,8 +32,8 @@ def index():
     return send_file('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,use_debugger=True,host='0.0.0.0',port=3000)
-    # app.run(debug=False,use_debugger=False,host='0.0.0.0',port=8100)
+    # app.run(debug=True,use_debugger=True,host='0.0.0.0',port=3000)
+    app.run(debug=False,use_debugger=False,host='0.0.0.0',port=3000)
 
     
 
