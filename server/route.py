c87a7754c7b55f20a2ff941cf1fcd544f347c282
wangkechun <978750903@qq.com>
2014-07-04 21:03:13 +0800

    添加超时

--- server/route.py
+++ server/route.py
@@ -15,19 +15,19 @@ import ocr
 
 def get_student_info(id,name,idcard):
     id = str(id)
-    # if int(id[2])>3: base_url = 'http://xxjw.hnust.cn/xxjw'
-    # else : base_url = 'http://kdjw.hnust.cn/kdjw'
+    if int(id[2])>3: base_url = 'http://xxjw.hnust.cn/xxjw'
+    else : base_url = 'http://kdjw.hnust.cn/kdjw'
 
-    if int(id[2])>3: base_url = 'http://localhost:2167/xxjw'
-    else : base_url = 'http://localhost:2169/kdjw'
+    # if int(id[2])>3: base_url = 'http://localhost:2167/xxjw'
+    # else : base_url = 'http://localhost:2169/kdjw'
 
     opener = requests.Session()
     url_img = base_url+'/verifycode.servlet'
-    try: opener.get(base_url)
+    try: opener.get(base_url,timeout=1)
     except: return {'error':True,'msg':"7.服务器网络故障，可能查询的人太多了，亲再等等"}
 
     for i in range(5):
-        try: img = opener.get(url_img).content
+        try: img = opener.get(url_img,timeout=1).content
         except : continue
         code = ocr.recognize(img)
         url_submit = base_url+'/xscjcx_check.jsp'
@@ -36,14 +36,14 @@ def get_student_info(id,name,idcard):
             'xssfzh':idcard,
             'yzm':code,
         }
-        try: res = opener.post(url_submit,payload).text
+        try: res = opener.post(url_submit,payload,timeout=1).text
         except : continue
         if 'yzm_guoq' in res : return {'error':True,'msg':"1.验证码过期"}
         if 'yzm_cuowu' in res : continue
         if 'notQueryXs' in res : return {'error':True,'msg':"3.未找到您输入信息的学生"}
         if 'systemError' in res : return {'error':True,'msg':"4.您访问的功能出现错误"}
         url_scope = base_url+'/xscjcx.jsp?yzbh=' + res[-36:-4]
-        try: html = opener.get(url_scope).text
+        try: html = opener.get(url_scope,timeout=1).text
         except : continue
         if not re.search('\d{10}',html) : return {'error':True,'msg':"5.登陆成功了，但是似乎没有你的信息哦"}
         return htmlToJson.htmlToJson(html)
